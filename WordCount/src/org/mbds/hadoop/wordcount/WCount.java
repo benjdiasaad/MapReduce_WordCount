6c20c67101ae22b03938e61a2a43f935713547f6
benjdia <cravatas90@gmail.com>
2020-12-27 14:35:14 +0100

    add configuration Hadoop to main class

--- WordCount/src/org/mbds/hadoop/wordcount/WCount.java
+++ WordCount/src/org/mbds/hadoop/wordcount/WCount.java
@@ -1,5 +1,23 @@
 package org.mbds.hadoop.wordcount;
 
-public class WCount {
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.util.GenericOptionsParser;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.IntWritable;
 
+
+public class WCount
+{
+	
+	public static void main(String[] args) throws Exception
+	{
+		
+		Configuration conf=new Configuration();
+		String[] ourArgs=new GenericOptionsParser(conf, args).getRemainingArgs();
+		
+	}
 }
