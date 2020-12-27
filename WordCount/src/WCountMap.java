6f94591d4ff15603a8a50a1561f3e1f819ca118f
benjdia <cravatas90@gmail.com>
2020-12-27 14:02:04 +0100

    Create Map Class

---
+++ WordCount/src/WCountMap.java
@@ -0,0 +1,32 @@
+/*
+  M2 MBDS - Big Data/Hadoop
+	Année 2013/2014
+  --
+  TP1: exemple de programme Hadoop - compteur d'occurences de mots.
+  --
+  WCountMap.java: classe MAP.
+*/
+package org.mbds.hadoop.wordcount;
+
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.IntWritable;
+import java.util.StringTokenizer;
+import org.apache.hadoop.mapreduce.Mapper;
+import java.io.IOException;
+
+
+	public class WCountMap extends Mapper<Object, Text, Text, IntWritable>
+	{
+		private static final IntWritable ONE=new IntWritable(1);
+
+		protected void map(Object key, Text value, Context context) throws IOException, InterruptedException
+		{
+			StringTokenizer tok=new StringTokenizer(value.toString(), " ");
+			while(tok.hasMoreTokens())
+			{
+				Text word=new Text(tok.nextToken());
+				context.write(word, ONE);
+			}
+		}
+	}
